1757a602bffcc46254ac4d4f42cfaed944e70975
Florin Constantin <103195148+codingisreallyhard@users.noreply.github.com>
2022-10-04 14:49:28 +0300

    Delete React-Test-Project directory

--- React-Test-Project/src/pages/ProductListPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProductsList from "../Products/ProductsList/ProductsList";
-
-function ProductListPage() {
-  return <ProductsList />;
-}
-
-export default ProductListPage;
